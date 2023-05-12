eecadb2b0ec03b6fd7861035a87929ae0e874498
Himanshu Kainthola <hkainthola6@gmail.com>
2023-05-12 23:14:10 +0530

    commit 3

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ fetch('https://restcountries.com/v3.1/all')
     .then((res) => res.json())
     .then((data) => {
         data.forEach((country) => {
-            console.log(country)
+
             const countryCard = document.createElement('a')
             countryCard.classList.add('country-card')
+            countryCard.href = `/country.html?name=${country.name.common}`
 
             countryCard.innerHTML = `
-                <img src="${country.flags.svg}" alt="flag">
+                <img src="${country.flags.svg}" alt="${country.flags.alt}">
                 <div class="card-text">
                     <h3 class="card-tittle">${country.name.common}</h3>
                     <p><b>Population</b>: ${country.population.toLocaleString('en-EU')}</p>
