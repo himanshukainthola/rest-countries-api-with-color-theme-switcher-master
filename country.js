3dff92b991e0e0db19761d38147ed9bb08eda8a1
Himanshu Kainthola <hkainthola6@gmail.com>
2023-05-21 17:01:23 +0530

    commit5

--- country.js
+++ country.js
@@ -9,12 +9,11 @@ const capital = document.querySelector('.capital')
 const topLevelDomain = document.querySelector('.top-level-domain')
 const currencies = document.querySelector('.currencies')
 const languages = document.querySelector('.languages')
-
+const borderCountries = document.querySelector('.border-countries')
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     .then((res) => res.json())
     .then(([country]) => {
-        console.log(country)
         flagsImg.src = country.flags.svg
         countryTitle.innerText = country.name.common
 
@@ -45,5 +44,19 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
 
         if (country.languages) {
             languages.innerText = Object.values(country.languages).join(', ')
+        }
+
+        if (country.borders) {
+            country.borders.forEach((border) => {
+                fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                    .then((res) => res.json())
+                    .then(([borderCountry]) => {
+
+                        const borderCountryTag = document.createElement('a')
+                        borderCountryTag.innerText = borderCountry.name.common
+                        borderCountryTag.href = `country.html?name=${borderCountry.name.common}`
+                        borderCountries.append(borderCountryTag)
+                    })
+            });
         }
     })
--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const countriesContainer = document.querySelector('.countries-containter')
+const filterByRegion = document.querySelector('#filter-by-region');
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-switcher');
+
+
+let allCountriesData
+
 
 fetch('https://restcountries.com/v3.1/all')
     .then((res) => res.json())
     .then((data) => {
-        data.forEach((country) => {
+        renderCountries(data)
+        allCountriesData = data
+    })
 
-            const countryCard = document.createElement('a')
-            countryCard.classList.add('country-card')
-            countryCard.href = `/country.html?name=${country.name.common}`
+filterByRegion.addEventListener('change', (e) => {
+    fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+        .then((res) => res.json())
+        .then(renderCountries)
 
-            countryCard.innerHTML = `
+})
+
+
+function renderCountries(data) {
+    countriesContainer.innerHTML = " "
+    data.forEach((country) => {
+
+        const countryCard = document.createElement('a')
+        countryCard.classList.add('country-card')
+        countryCard.href = `/country.html?name=${country.name.common}`
+
+        countryCard.innerHTML = `
                 <img src="${country.flags.svg}" alt="${country.flags.alt}">
                 <div class="card-text">
                     <h3 class="card-tittle">${country.name.common}</h3>
@@ -18,11 +39,17 @@ fetch('https://restcountries.com/v3.1/all')
                     <p><b>Capital</b>: ${country.capital}</p>
                 </div>
             `
-            countriesContainer.append(countryCard)
+        countriesContainer.append(countryCard)
 
 
-        })
     })
+}
 
+searchInput.addEventListener('input', (e) => {
+    const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+    renderCountries(filteredCountries)
+})
 
-
+themeChanger.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
